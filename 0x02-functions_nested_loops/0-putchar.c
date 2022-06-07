aed6ad4ef8dc240e65f14dc564f71e3c80f06c60
Incogany <tigerclaw373@gmail.com>
2022-06-07 08:54:13 -0700

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	char *i= "Main";
-
-	while (*i)
-	{
-		_putchar(*i);
-		i++;
-	}
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);

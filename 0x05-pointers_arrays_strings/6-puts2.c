60f90b93d06e7b3fa4303840dc50299e85f22659
Incogany <tigerclaw373@gmail.com>
2022-06-13 08:56:04 -0700

    a

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: the string to be treated
+ *
+ * Return: void
+ */
+{
+	int i;
+	int j = 0;
+
+	while puts2(char *str)
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}

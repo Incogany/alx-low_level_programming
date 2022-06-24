5b30d592572fdb0fbfd24f1ab99e39afb2158f10
Incogany <tigerclaw373@gmail.com>
2022-06-24 13:09:05 -0700

    long

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - prints minimum number of coins to make change for an amount of money
  * @argc: number of argumnents passed
@@ -17,6 +17,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
+
 	num = atoi(argv[1]);
 	result = 0;
 

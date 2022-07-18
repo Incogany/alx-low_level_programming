3d246212d01ebf0d39883813433cec6a6a6f6938
Incogany <tigerclaw373@gmail.com>
2022-07-18 11:22:32 -0700

    woo

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,7 +8,7 @@
  * @h: name of the list
  * Return: the number of nodes
  */
-size_t print_listint(const litint_t *h)
+size_t print_listint(const listint_t *h)
 {
 	int count = 0;
 
--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
 	listint_t *tmp_node = *head;
-	listint-t *after_node;
+	listint_t *after_node;
 
 	if (*head == NULL)
 		return (-1);

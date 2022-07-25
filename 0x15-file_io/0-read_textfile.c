df4df00265d32e51431de7c13e203d1e27854350
Incogany <tigerclaw373@gmail.com>
2022-07-25 12:54:52 -0700

    mg

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * read_textfile - function reads a text file and prints it to statndard output
+ * @filename: file
+ * @letters: number of letters read and printed, 0 if fall
+ *
+ * Return: number of letters read and printed, 0 if fail
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, n_read, n_writer;
+	char *s;
+
+	if (filename == NULL)
+		return (0);
+	s = malloc(letters * sizeof(char) + 1);
+	if (!s)
+		return (0);
+	fd = open(filename, o_RDONLY);
+	if (fd == -1)
+	{
+		free(s);
+		return (0);
+	}
+	n_read = read(fd, s, letters);
+	if (n_read == -1)
+	{
+		free(s)
+			return (0);
+	}
+	n_write = write(STDOUT_FILEND, s, n_read);
+	close(fd);
+	free(s);
+	if (n_read == n_write)
+		return (n_write);
+	return(0);
+}

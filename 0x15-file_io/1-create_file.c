9a8a14d666696ba4821f598069989570235e923a
Incogany <tigerclaw373@gmail.com>
2022-07-25 23:22:50 -0700

    mg

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,19 +9,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i = 0, n_write;
-
+	int i = 0, file;
+	
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content == NULL)
 		text_content = "";
-	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
-	if (fd == -1)
-		return (-1);
+
+
 	while (text_content[i] != '\0')
+	{
 		i++;
-	n_write == write(fd, text_content, i);
-	if (n_write == -1)
+	}
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
 		return (-1);
+
+	write(file, text_content, i);
+
 	return (1);
 }
